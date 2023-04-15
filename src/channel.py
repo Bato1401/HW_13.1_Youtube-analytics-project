7d38eb2ee6ee73112926c9286780e99e17c66978
Bato1401 <lego.1401@gmail.com>
2023-04-15 18:03:11 +0300

    add @classmethod get_service

--- src/channel.py
+++ src/channel.py
@@ -2,7 +2,6 @@ import os
 
 from googleapiclient.discovery import build
 
-
 api_key: str = os.getenv('API_KEY_YOUTUBE')
 
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -19,3 +18,18 @@ class Channel:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         print(channel)
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод возвращающий объект для работы с YouTube API"""
+        channel = youtube.channels().list(id=api_key, part='snippet,statistics').execute()
+        return channel
+
+    @staticmethod
+    def to_json():
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+
+
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+
+print(vdud.get_service())
